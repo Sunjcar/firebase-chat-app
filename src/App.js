2dc81662a7aa25b24e37386b2f18c7281b64690b
Raymond Chen <raychen358@gmail.com>
2022-10-18 22:34:45 -0700

    Sign In displays side bar and chatroom

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from './Utilities/firebasekey';
 import Sidebar from './Components/Sidebar';
 import SignIn from './Components/SignIn';
+import Main from './Components/Main';
 
 const App = () => {
   const [user] = useAuthState(auth);
   return (
     <div className='flex flex-col items-center justify-center min-h-screen bg-white dark:bg-slate-800'>
-      <Nav/>
+      <Nav />
       <section className='flex flex-col justify-center h-screen bg-white dark:bg-slate-800'>
-        {user ? <Sidebar/> : <SignIn/>}
+        {user ? <Sidebar /> : <></>}
+        {user ? <Main /> : <SignIn />}
       </section>
     </div>
   )
