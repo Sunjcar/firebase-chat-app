cf39e0284c5b33bd543c462272bf995cf8236431
Raymond Chen <raychen358@gmail.com>
2022-10-22 23:34:41 -0700

    Display Messages and Delete doc

--- src/Components/Messages.js
+++ src/Components/Messages.js
@@ -1,17 +1,26 @@
 import React from 'react'
-import { auth } from '../Utilities/firebasekey';
+import { auth, db } from '../Utilities/firebasekey';
+import {RiDeleteBin6Line} from 'react-icons/ri'
+import { deleteDoc, doc } from 'firebase/firestore';
 
-const Messages = (props) => {
-    const { text, uid, photoURL } = props.message;
+const Messages = ({ message }) => {
+  const messageClass = message.uid === auth.currentUser.uid ? 'sent' : 'received';
 
-    const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
-  
-    return (<>
-      <div className={`message ${messageClass}`}>
-        <img src={photoURL} alt={'avatar'}/>
-        <p>{text}</p>
+  const handleDelete = async () => {
+    const docRef = doc(db,'messages', message.id)
+   await deleteDoc(docRef)
+}
+  return (
+    <>
+      <div className={`message ${messageClass}  `}>
+        <img src={message.photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} alt={'avatar'} />
+        <div className='relative'>
+          <p>{message.text}</p>
+          <button className='absolute right-3 bottom-3' onClick={() => handleDelete()}> <RiDeleteBin6Line/></button>
+        </div>
       </div>
-    </>)
-  }
+    </>
+  )
+}
 
 export default Messages
