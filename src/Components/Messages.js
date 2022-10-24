98a425f227da4461d0773dc295a34308520291c5
Raymond Chen <raychen358@gmail.com>
2022-10-23 20:49:38 -0700

    delete document function

--- src/Components/Messages.js
+++ src/Components/Messages.js
@@ -1,22 +1,27 @@
 import React from 'react'
 import { auth, db } from '../Utilities/firebasekey';
-import {RiDeleteBin6Line} from 'react-icons/ri'
-import { deleteDoc, doc } from 'firebase/firestore';
+import { RiDeleteBin6Line } from 'react-icons/ri'
+import {deleteDoc, doc, serverTimestamp, setDoc } from 'firebase/firestore';
 
 const Messages = ({ message }) => {
   const messageClass = message.uid === auth.currentUser.uid ? 'sent' : 'received';
 
   const handleDelete = async () => {
-    const docRef = doc(db,'messages', message.id)
-   await deleteDoc(docRef)
-}
+    const docRef = doc(db, 'messages', message.id)
+    await deleteDoc(docRef)
+  }
+
   return (
     <>
       <div className={`message ${messageClass}  `}>
         <img src={message.photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} alt={'avatar'} />
-        <div className='relative'>
-          <p>{message.text}</p>
-          <button className='absolute right-3 bottom-3' onClick={() => handleDelete()}> <RiDeleteBin6Line/></button>
+        <div className='flex gap-4'>
+          <p>
+            {message.text}
+          </p>
+          <button onClick={() => handleDelete()}>
+            <RiDeleteBin6Line className='text-xl text-indigo-500 hover:rounded-xl hover:bg-red-600' />
+          </button>
         </div>
       </div>
     </>
